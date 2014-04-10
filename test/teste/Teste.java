9916f002865343bf8a9b4e84f9bab0dfc6dd1bc7
Dyego <dyego.maas@gmail.com>
2014-04-10 19:30:17 -0300

    Adicionado projeto de teste e JUnit.

---
+++ test/teste/Teste.java
@@ -0,0 +1,13 @@
+package teste;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Teste extends TestCase{
+
+	@Test
+	public void teste_soma(){
+		assertTrue(1 > 0);
+	}
+}
