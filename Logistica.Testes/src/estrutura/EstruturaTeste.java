94334947b7d5a236753faf927988a3ee443fbf0f
Holmed <dyego.maas@gmail.com>
2014-04-24 22:44:34 -0300

    DM:4: Alterada estrutura do projeto.

---
+++ Logistica.Testes/src/estrutura/EstruturaTeste.java
@@ -0,0 +1,27 @@
+package estrutura;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import estrutura.Aresta;
+import estrutura.Vertice;
+
+public class EstruturaTeste {
+	
+	@Test
+	public void um_vertice_possui_quatro_arestas(){		
+		Aresta norte = new Aresta();
+		Aresta sul = new Aresta(); 
+		Aresta leste = new Aresta();
+		Aresta oeste = new Aresta(); 
+		 
+		Vertice vertice = new Vertice(norte, sul, leste, oeste);
+		
+		assertEquals(norte, vertice.getNorte());
+		assertEquals(sul, vertice.getSul());
+		assertEquals(leste, vertice.getLeste());
+		assertEquals(oeste, vertice.getOeste());
+	}
+	
+}
---
+++ Logistica.Testes/src/estrutura/Teste.java
@@ -0,0 +1,13 @@
+package estrutura;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Teste extends TestCase {
+
+	@Test
+	public void teste_soma(){
+		assertTrue(1 > 0);
+	}
+}
