3fe347235bb6cd3eeaa8ba1dd121e5cd33e58aa9
Dyego <dyego.maas@gmail.com>
2014-04-28 13:31:01 -0300

    DM: alterado GeradorPedidos para implementar Runnable.

--- Logistica.Core/src/pedidos/geracao/GeradorPedidos.java
+++ Logistica.Core/src/pedidos/geracao/GeradorPedidos.java
@@ -8,7 +8,7 @@ import pedidos.Pedido;
 import pedidos.recepcao.IRecebedorPedidos;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class GeradorPedidos extends Thread{
+public class GeradorPedidos implements Runnable{
 	
 	private boolean gerarPedidos = true;
 	private IRecebedorPedidos recebedorPedidos;
@@ -23,7 +23,7 @@ public class GeradorPedidos extends Thread{
 			recebedorPedidos.receberPedido(novoPedido());
 			
 			try {
-				sleep(50);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
