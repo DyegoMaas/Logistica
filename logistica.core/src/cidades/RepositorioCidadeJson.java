e35041cc9e8eb573f4064afdd8a65373f83e91f4
tintiz00rd <angelo.tinti@gmail.com>
2014-04-29 01:31:40 -0300

    AT:Tentativa de configurar o log4j.

--- logistica.core/src/cidades/RepositorioCidadeJson.java
+++ logistica.core/src/cidades/RepositorioCidadeJson.java
@@ -2,8 +2,10 @@ package cidades;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.net.URI;
 import java.util.UUID;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
 
@@ -11,6 +13,8 @@ import estrutura.Cidade;
 
 public class RepositorioCidadeJson implements IRepositorioCidade {
 
+	static final Logger logger = LogManager.getLogger(RepositorioCidadeJson.class.getName());
+	
 	@Override
 	public Cidade obterCidade(UUID id) {
 		// TODO Auto-generated method stub
@@ -19,10 +23,10 @@ public class RepositorioCidadeJson implements IRepositorioCidade {
 
 	@Override
 	public void salvarCidade(Cidade cidade) throws Exception {
+		logger.debug("Salvando cidade com id = " + cidade.getId().toString());
 		if(cidade.getId() == null)
 			throw new Exception("Propriedade id não especificada.");
-		String caminhoProjeto = getClass().getClassLoader().getResource(".").getPath();
-		String caminhoArquivo = caminhoProjeto + "/fixtures/cidades/" + cidade.getId();
+		String caminhoArquivo = "C:\\dev\\java\\fixtures\\cidades\\" + cidade.getId();
 		File arquivoJsonCidade = new File(caminhoArquivo);
 		if(arquivoJsonCidade.createNewFile()){
 			Gson gson = new Gson();
@@ -31,5 +35,6 @@ public class RepositorioCidadeJson implements IRepositorioCidade {
 			fileWriter.write(jsonCidade);
 			fileWriter.close();
 		}
+		logger.debug("Finalizou salvamento de cidade com id = " + cidade.getId().toString());
 	}
 }
